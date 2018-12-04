07f0bd867f11790ceaf9059cde7c38d210abfe5c
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-04 18:38:24 +0100

    feat: too complicated

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Key from "../Key";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-const Row = styled.div`
+const KeyboardSection = styled.div`
   position: relative;
-  /* padding-top: 33%; */
+  margin: 0 5%;
   flex-grow: 1;
   height: 100%;
 `;
 
 const KeyboardTable = styled.div`
   display: flex;
-  height: 50%;
+  height: 100%;
 `;
 
 const Layout = styled.div`
@@ -22,47 +23,61 @@ const Layout = styled.div`
 
 const keyMapping = {
   0: () => null,
-  1: ({ idx, ...props }) => <Key key={idx} {...props} verticalSpan={1} />,
-  2: ({ idx, ...props }) => <Key key={idx} {...props} verticalSpan={2} />,
-  3: ({ idx, ...props }) => (
-    <Key key={idx} {...props} verticalSpan={2} lift={1} />
-  )
+  1: props => <Key {...props} verticalSpan={1} />,
+  2: props => <Key {...props} verticalSpan={2} />,
+  3: props => <Key {...props} verticalSpan={2} lift={1} />
 };
 
-const makeRow = onClick => height => width => row => (keyType, idx) =>
+const makeRow = onClick => rowLength => height => width => offset => row => (
+  keyType,
+  idx
+) =>
+  console.log(`${offset}${row}${idx}`) ||
   keyMapping[keyType]({
     width,
     height,
     onClick,
     row,
     column: idx,
-    idx,
+    key: `${offset}${row}${idx}`,
+    id: idx + row * rowLength + offset,
     label: `${row}x${idx}`
   });
 
 // generate rows with
 // JSON.stringify("x12345x".split("").map(label =>({label, verticalSpan: 1}))).replace('"label"','label').slice(1,-1)
-export default ({ onKeySelect, layout }) => {
-  const rows = layout.left.length;
+const Keyboard = ({ onKeySelect, layout, split }) => {
+  const rows = layout.length;
+  const splitOffset = rows / 2; // Just in case of split keyboard
+  const rowLn = layout[0].length; // Fixed to first row width
   const height = 100 / rows;
-  const width = 100 / layout.left[0].length; // Fixed to first row width
-  const rower = makeRow(onKeySelect)(height)(width);
+  const width = 100 / rowLn;
+  const rower = makeRow(onKeySelect)(rowLn)(height)(width);
+  const hands = split
+    ? [layout.slice(0, splitOffset), layout.slice(splitOffset)]
+    : layout;
   return (
     <Layout>
       <KeyboardTable>
-        <Row style={{ marginRight: "5%" }}>
-          {layout.left.map((keys, rowIdx) => {
-            const makeKey = rower(rowIdx);
-            return keys.map(makeKey);
-          })}
-        </Row>
-        <Row style={{ marginLeft: "5%" }}>
-          {layout.right.map((keys, rowIdx) => {
-            const makeKey = rower(rowIdx);
-            return keys.map(makeKey);
-          })}
-        </Row>
+        {hands.map((hand, side) => {
+          const keyWithOffset = rower(side * splitOffset * rowLn);
+          return (
+            <KeyboardSection key={`hand ${side}`}>
+              {hand.map((keys, rowIdx) => {
+                const makeKey = keyWithOffset(rowIdx);
+                return keys.map(makeKey);
+              })}
+            </KeyboardSection>
+          );
+        })}
       </KeyboardTable>
     </Layout>
   );
 };
+
+Keyboard.propTypes = {
+  split: PropTypes.bool,
+  layout: PropTypes.arrayOf(PropTypes.array)
+};
+
+export default Keyboard;
