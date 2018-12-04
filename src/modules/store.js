7dcbc9a90b12823e1f7e3fe2fb64b6aa863c821c
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-04 13:46:21 +0100

    feat: add redux devtools

--- src/modules/store.js
+++ src/modules/store.js
@@ -6,4 +6,13 @@ const rootReducer = combineReducers({
   keyboard
 });
 
-export const createStore = () => createReduxStore(rootReducer, {});
+export const createStore = () => {
+  const windowGlobal = typeof window !== "undefined" && window;
+
+  const devtools =
+    process.env.NODE_ENV === "development" && windowGlobal.devToolsExtension
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f;
+  return createReduxStore(rootReducer, {}, devtools);
+};
