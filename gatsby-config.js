72c04ebec54f96edba5295dbce51cea65e4c1931
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-09 09:30:17 +0100

    refactor: delete unnecessary elements + manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  siteMetadata: {
+    title: "Eregodox Config"
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        // icon: "src/images/gatsby-icon.png", // This is relative to the root of the site.
+        short_name: "ErgoConf",
+        name: "Ergodox layout creator",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/png"
+          },
+          {
+            src: "favicon-512.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ],
+        start_url: "."
+      }
+    }
+  ]
 };
