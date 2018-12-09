aa1f4ad48db0cd8e9e7f12be8755994cb8fd9292
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-09 10:08:45 +0100

    feat: getting ready for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,23 +10,12 @@ module.exports = {
         background_color: "#663399",
         theme_color: "#663399",
         display: "minimal-ui",
-        // icon: "src/images/gatsby-icon.png", // This is relative to the root of the site.
+        icon: "src/images/favicon.png", // This is relative to the root of the site.
         short_name: "ErgoConf",
         name: "Ergodox layout creator",
-        icons: [
-          {
-            src: "favicon.png",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/png"
-          },
-          {
-            src: "favicon-512.png",
-            type: "image/png",
-            sizes: "512x512"
-          }
-        ],
         start_url: "."
       }
-    }
+    },
+    "gatsby-plugin-offline"
   ]
 };
