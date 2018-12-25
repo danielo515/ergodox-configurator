64a79007cb79f6851792bf230c33565c847b6409
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-25 03:18:34 +0100

    feat: improve ts support

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   transform: {
-    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
+    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js",
+    "^.+\\.(tsx?|jsx?)$": "ts-jest"
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js"
   },
+  moduleFileExtensions: ["ts", "tsx", "js"],
   testPathIgnorePatterns: ["node_modules", ".cache"],
   transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
   globals: {
