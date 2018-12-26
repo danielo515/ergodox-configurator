d201ce6f0f650813033441ab0db83cc949247b50
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-12-26 14:46:49 +0100

    fix: global regex import

--- src/modules/keyboard/parseLayout.js
+++ src/modules/keyboard/parseLayout.js
@@ -4,7 +4,7 @@ export const splitKeysRegex = /,\s*(?![^()]*\))/;
  *  Regex to find the keycodes inside of a keycode array like this:
  * [2] = LAYOUT_ergodox(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5)
  **/
-const fullLayoutRegex = /(?:\[[0-9]+\]\s*=\s*[^(]*\()?([\w\d\(,\)\n\s\t]*)\),/gm;
+const fullLayoutRegex = /(?:\[[0-9]+\]\s*=\s*[^(]*\()?([\w\d\(,\)\n\s\t]*)\),/;
 // given an string representing an array of keycodes returns an array of each entry which can be a single keycode like KC_A or a composite one TD(copy)
 export const splitEntries = str => str.split(splitKeysRegex);
 // given a simple modifier key eg TO(1) it returns an object with code eg TD and params eg 1
@@ -36,6 +36,11 @@ const reduceSkipping = toSkip => fn => init => arr => {
 
 const flatten = arr => arr.reduce((a, b) => a.concat(b), []);
 
+/**
+ * Given a layout description (array of arrays) and a set of keys
+ * puts the keys on the expected shape according to the layout.
+ * @param {Array[Int[]]} layout
+ */
 export const keysToLayout = layout => keys => {
   return reduceSkipping(0)((acc, _, idx, offset) => {
     acc[idx] = { ...keys[idx - offset], id: idx };
@@ -43,5 +48,8 @@ export const keysToLayout = layout => keys => {
   })({})(flatten(layout));
 };
 
-export const parseLayout = keyCodes => layoutStr =>
-  splitEntries(layoutStr).map(identifyKey(keyCodes));
+export const parseLayout = keyCodes => layoutStr => {
+  const regexRes = fullLayoutRegex.exec(layoutStr);
+  const toParse = regexRes ? regexRes[1] : layoutStr; // check if the full layout matches, then use the captured layout
+  return splitEntries(toParse).map(identifyKey(keyCodes));
+};
